5518aa51c818d761588ebdf11e74646af34e2b00
ruheyun <1654033661@qq.com>
2026-04-30 14:31:02 +0800

    20260430u

--- models/gaussian_diffusion.py
+++ models/gaussian_diffusion.py
@@ -282,7 +282,7 @@ class GaussianDiffusion(torch.nn.Module):
 
             total_loss_gauss = torch.zeros(b, device=device)
             for _ in range(self.dp_params['noise_multiplicity_K']):
-                t, pt = self.sample_time(b, device, 'ada')
+                t, pt = self.sample_time(b, device, 'uniform')
                 noise = torch.randn_like(x)
                 x_t = self.gaussian_q_sample(x, t, noise=noise)
 
