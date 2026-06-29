78eee45bdd24df9298e9a6a3bfd425f58f8c4bc6
ruheyun <1654033661@qq.com>
2026-06-29 13:47:42 +0800

    20260629

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -76,6 +76,6 @@ def data_process(data_path, exp_path, num_encoder='quantile', cat_encoder='alb')
 
 
 if __name__ == '__main__':
-    data_path = 'data/cardio'
-    exp_path = 'exp/cardio'
+    data_path = 'data/credit'
+    exp_path = 'exp/credit'
     data_process(data_path, exp_path, num_encoder='minmax', cat_encoder='alb')
